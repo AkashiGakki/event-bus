3b87c567aa34a416fbca377f7649167a0c3c9b3e
Akashi <akashi_sai@163.com>
2022-07-08 00:10:22 +0800

    :white_check_mark: chore: cost test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,16 @@
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import bus from '../src'
 
 describe('should', () => {
   it('exported', () => {
     const ret: any = bus.$on('test', () => { })
 
-    const fn = () => {}
+    const fn = () => { }
     bus.$on('test', fn)
     bus.$off('test', fn)
 
     expect(ret._events).toMatchInlineSnapshot(
-    `
+      `
       {
         "test": [
           [Function],
--- test/test.cost.ts
+++ test/test.cost.ts
@@ -1,14 +1,14 @@
 import bus from '../src'
 
-console.time('test on');
+console.time('test on')
 for (let i = 0; i < 1000; i++)
-  bus.$on(`${i}`, () => { });
-console.timeEnd('test on');
+  bus.$on(`${i}`, () => { })
+console.timeEnd('test on')
 
-console.time('test emit');
+console.time('test emit')
 for (let i = 0; i < 1000; i++)
-  bus.$emit(`${i}`);
-console.timeEnd('test emit');
+  bus.$emit(`${i}`)
+console.timeEnd('test emit')
 
 // bus.$on('test', (ev: any) => console.log('ev:', ev))
 // bus.$emit('test', { akashi: 'asuak' })
