c64384350c4f029e198608443c2315f5962742c7
Akashi <akashi_sai@163.com>
2022-07-09 14:28:30 +0800

    :package: chore: update deps && test

--- test/test.cost.ts
+++ test/test.cost.ts
@@ -10,5 +10,10 @@ for (let i = 0; i < 1000; i++)
   bus.$emit(`${i}`)
 console.timeEnd('test emit')
 
+console.time('test off')
+for (let i = 0; i < 1000; i++)
+  bus.$off(`${i}`)
+console.timeEnd('test off')
+
 // bus.$on('test', (ev: any) => console.log('ev:', ev))
 // bus.$emit('test', { akashi: 'asuak' })
